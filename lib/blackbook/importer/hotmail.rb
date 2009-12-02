26e0712b67188d8d88b11968cb842f1c6fef6507
Sergey Ustinov <sergey.ustinov@flatsoft.com>
2009-12-02 17:19:15 +0300

    fixed bug with hormail and localization

--- lib/blackbook/importer/hotmail.rb
+++ lib/blackbook/importer/hotmail.rb
@@ -4,19 +4,19 @@ require 'cgi'
 ##
 # imports contacts for MSN/Hotmail
 class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
-
-  DOMAINS = { "compaq.net"        => "https://msnia.login.live.com/ppsecure/post.srf",
-              "hotmail.co.jp"     => "https://login.live.com/ppsecure/post.srf",
-              "hotmail.co.uk"     => "https://login.live.com/ppsecure/post.srf",
-              "hotmail.com"       => "https://login.live.com/ppsecure/post.srf",
-              "hotmail.de"        => "https://login.live.com/ppsecure/post.srf",
-              "hotmail.fr"        => "https://login.live.com/ppsecure/post.srf",
-              "hotmail.it"        => "https://login.live.com/ppsecure/post.srf",
+  DOMAINS = { "compaq.net" => "https://msnia.login.live.com/ppsecure/post.srf",
+              "hotmail.co.jp" => "https://login.live.com/ppsecure/post.srf",
+              "hotmail.co.uk" => "https://login.live.com/ppsecure/post.srf",
+              "hotmail.com" => "https://login.live.com/ppsecure/post.srf",
+              "hotmail.de" => "https://login.live.com/ppsecure/post.srf",
+              "hotmail.fr" => "https://login.live.com/ppsecure/post.srf",
+              "hotmail.it" => "https://login.live.com/ppsecure/post.srf",
+              "live.com" => "https://login.live.com/ppsecure/post.srf",
               "messengeruser.com" => "https://login.live.com/ppsecure/post.srf",
-              "msn.com"           => "https://msnia.login.live.com/ppsecure/post.srf",
-              "passport.com"      => "https://login.live.com/ppsecure/post.srf",
-              "webtv.net"         => "https://login.live.com/ppsecure/post.srf" }
-              
+              "msn.com" => "https://msnia.login.live.com/ppsecure/post.srf",
+              "passport.com" => "https://login.live.com/ppsecure/post.srf",
+              "webtv.net" => "https://login.live.com/ppsecure/post.srf" }
+
   ##
   # Matches this importer to an user's name/address
 
@@ -25,13 +25,13 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
     domain = username_domain(options[:username].downcase)
     !domain.empty? && DOMAINS.keys.include?( domain ) ? true : false
   end
-   
+
   ##
   # Login procedure
   # 1. Go to login form
   # 2. Set login and passwd
   # 3. Set PwdPad to IfYouAreReadingThisYouHaveTooMuchFreeTime minus however many characters are in passwd (so if passwd
-  #    was 8 chars, you'd chop 8 chars of the end of IfYouAreReadingThisYouHaveTooMuchFreeTime - giving you IfYouAreReadingThisYouHaveTooMuch)
+  # was 8 chars, you'd chop 8 chars of the end of IfYouAreReadingThisYouHaveTooMuchFreeTime - giving you IfYouAreReadingThisYouHaveTooMuch)
   # 4. Set the action to the appropriate URL for the username's domain
   # 5. Get the query string to append to the new action
   # 5. Submit the form and parse the url from the resulting page's javascript
@@ -40,30 +40,30 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
   def login
     page = agent.get('http://login.live.com/login.srf?id=2')
     form = page.forms.first
-    form.login   = options[:username]
-    form.passwd  = options[:password]
-    form.PwdPad  = ( "IfYouAreReadingThisYouHaveTooMuchFreeTime"[0..(-1 - options[:password].to_s.size )])
+    form.login = options[:username]
+    form.passwd = options[:password]
+    form.PwdPad = ( "IfYouAreReadingThisYouHaveTooMuchFreeTime"[0..(-1 - options[:password].to_s.size )])
     query_string = page.body.scan(/g_QS="([^"]+)/).first.first rescue nil
-    form.action  = login_url + "?#{query_string.to_s}"
+    form.action = login_url + "?#{query_string.to_s}"
     page = agent.submit(form)
-    
+
     # Check for login success
     if page.body =~ /The e-mail address or password is incorrect/ ||
       page.body =~ /Sign in failed\./
-      raise( Blackbook::BadCredentialsError, 
+      raise( Blackbook::BadCredentialsError,
         "That username and password was not accepted. Please check them and try again." )
     end
-    
-    page = agent.get( page.body.scan(/http\:\/\/[^"]+/).first )
+
+    @first_page = agent.get( page.body.scan(/http\:\/\/[^"]+/).first )
   end
-  
+
   ##
   # prepare this importer
 
   def prepare
     login
   end
-  
+
   ##
   # Scrape contacts for Hotmail
   # Seems like a POST to directly fetch CSV contacts from options.aspx?subsection=26&n=
@@ -75,23 +75,47 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
       raise( Blackbook::BadCredentialsError, "Must be authenticated to access contacts." )
     end
 
-    page = agent.get('PrintShell.aspx?type=contact')
-    rows = page.search("//div[@class='ContactsPrintPane cPrintContact BorderTop']")
-    rows.collect do |row|
-      name = row.search("//div[@class='cDisplayName']").first.innerText.strip
-      
-      vals = {}
-      row.search("//table/tr").each do |pair|
-        key = pair.search("/td[@class='TextAlignRight Label']").first.innerText.strip
-        val = pair.search("/td[@class='Value']").first.innerText.strip
-        vals[key.to_sym] = val
+    page = agent.get('http://mail.live.com/')
+
+    if page.iframes.detect { |f| f.src =~ /\/mail\/TodayLight.aspx/ }
+      page = agent.get( page.iframes.first.src )
+
+      if button = page.forms.first.buttons.detect { |b| b.name == 'TakeMeToInbox' }
+        page = agent.submit( page.forms.first, button )
       end
-      vals[:name] = name
-      vals[:email] = (vals['Personal e-mail:'.to_sym] || vals['Work e-mail:'.to_sym]).split(' ').first rescue ''
-      vals
+    end
+
+    link_with_contacts = nil
+    
+    page.iframes.each  do |frame|
+      spage = agent.get( frame.src )
+
+      if (link_with_contacts = spage.link_with(:text => 'Contact list'))
+        break
+      elsif (link_with_contacts = spage.link_with(:href => /ContactMainLight/))
+        break
+      end
+    end
+   
+    return [] unless link_with_contacts
+    
+    page = link_with_contacts.click
+
+    contacts = parse_contacts(page.body)
+    while link = page.link_with(:text => 'Next page')
+      page = link.click
+      contacts += parse_contacts(page.body)
+    end
+
+    contacts
+  end
+
+  def parse_contacts(source)
+    source.scan(/ICc.*\:\[.*?,.*?,\['se','ct'\],'(.*?)',.*?,.*?,'(.*?)',.*\]/).collect do |name, email|
+      { :name => (name =~ /\\x26\\x2364\\x3b/ ? nil : name), :email => email.gsub(/\\x40/, '@') }
     end
   end
-  
+
   ##
   # lookup for the login service that should be used based on the user's
   # address
@@ -99,7 +123,7 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
   def login_url
     DOMAINS[username_domain] || DOMAINS['hotmail.com']
   end
-  
+
 
   ##
   # normalizes the host for the page that is currently being "viewed" by the
@@ -110,7 +134,7 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
     uri = agent.current_page.uri
     "#{uri.scheme}://#{uri.host}"
   end
-  
+
   ##
   # determines the domain for the user
 
@@ -119,6 +143,6 @@ class Blackbook::Importer::Hotmail < Blackbook::Importer::PageScraper
     return unless username
     username.to_s.split('@').last
   end
-  
+
   Blackbook.register(:hotmail, self)
 end
